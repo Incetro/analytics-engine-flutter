49f21127036670b8ec6f80503bef0c80126b4e02
lamafout <dzordzvashington@gmail.com>
2025-09-02 14:32:39 +0300

    feat: extented branch

--- lib/src/services/branch_service.dart
+++ lib/src/services/branch_service.dart
@@ -7,26 +7,192 @@ class BranchAnalyticsAdapter implements AnalyticsService {
 
   @override
   final AnalysisLogger logger;
-
   @override
   Future<void> logEvent(String name, {Map<String, Object>? params}) async {
-    var event = BranchEvent.customEvent(name);
+    try {
+      final rawParams = params ?? {};
 
-    if (params != null) {
-      for (var key in params.keys) {
-        event.addCustomData(key, params[key]);
+      switch (name.toLowerCase()) {
+        // Commerce events
+        case 'add_to_cart':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.ADD_TO_CART);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'add_to_wishlist':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.ADD_TO_WISHLIST);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'view_cart':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.VIEW_CART);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'initiate_purchase':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.INITIATE_PURCHASE);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'purchase':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.PURCHASE);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'click_ad':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.CLICK_AD);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'reserve':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.RESERVE);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'view_ad':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.VIEW_AD);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        // Content events
+        case 'search':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.SEARCH);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'view_item':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.VIEW_ITEM);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'view_items':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.VIEW_ITEMS);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'rate':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.RATE);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'share':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.SHARE);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        // User lifecycle events
+        case 'complete_registration':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.COMPLETE_REGISTRATION);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'complete_tutorial':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.COMPLETE_TUTORIAL);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'achieve_level':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.ACHIEVE_LEVEL);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'unlock_achievement':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.UNLOCK_ACHIEVEMENT);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'invite':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.INVITE);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'login':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.LOGIN);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'start_trial':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.START_TRIAL);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        case 'subscribe':
+          final event = BranchEvent.standardEvent(BranchStandardEvent.SUBSCRIBE);
+          _fillStandardFields(event, rawParams);
+          break;
+
+        default:
+          final event = BranchEvent.customEvent(name);
+          rawParams.forEach((k, v) => event.addCustomData(k, v));
+          FlutterBranchSdk.trackContentWithoutBuo(branchEvent: event);
+          break;
       }
-    }
 
-    FlutterBranchSdk.trackContentWithoutBuo(branchEvent: event);
-
-    logger.logSuccess(
-        text: 'Event sent', 
+      logger.logSuccess(
+        text: 'Event sent',
         source: 'Branch Adapter',
         eventName: name,
         params: params,
       );
+    } catch (e, stack) {
+      logger.logError(
+        source: 'Branch Adapter',
+        text: 'Failed to send event',
+        stack: stack,
+        eventName: name,
+        params: params,
+      );
+    }
   }
+
+  void _fillStandardFields(BranchEvent event, Map<String, Object> params) {
+    if (params['transaction_id'] != null) {
+      event.transactionID = params['transaction_id'].toString();
+    }
+    if (params['revenue'] != null) {
+      event.revenue = double.tryParse(params['revenue'].toString()) ?? -1;
+    }
+    if (params['currency'] != null) {
+      event.currency = params['currency'] == 'RUB' ?  BranchCurrencyType.RUB : params['currency'] == 'EUR' ? BranchCurrencyType.EUR : BranchCurrencyType.USD;
+    }
+    if (params['shipping'] != null) {
+      event.shipping = double.tryParse(params['shipping'].toString()) ?? -1;
+    }
+    if (params['tax'] != null) {
+      event.tax = double.tryParse(params['tax'].toString()) ?? -1;
+    }
+    if (params['coupon'] != null) {
+      event.coupon = params['coupon'].toString();
+    }
+    if (params['affiliation'] != null) {
+      event.affiliation = params['affiliation'].toString();
+    }
+    if (params['event_description'] != null) {
+      event.eventDescription = params['event_description'].toString();
+    }
+    if (params['search_query'] != null) {
+      event.searchQuery = params['search_query'].toString();
+    }
+
+    params.forEach((k, v) {
+      if (![
+        'transaction_id',
+        'revenue',
+        'currency',
+        'shipping',
+        'tax',
+        'coupon',
+        'affiliation',
+        'event_description',
+        'search_query',
+      ].contains(k)) {
+        event.addCustomData(k, v);
+      }
+    });
+
+    FlutterBranchSdk.trackContentWithoutBuo(branchEvent: event);
+  }
+
 
   @override
   Future<void> setUserId(String userId) async {
